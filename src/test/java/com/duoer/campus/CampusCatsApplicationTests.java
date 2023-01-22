85c1d7fc1ac8c3644890d3ca0537ffc513a73c69
duoer <1557030651@qq.com>
2023-01-22 11:53:48 +0800

    fix: 修正sql语法错误

--- src/test/java/com/duoer/campus/CampusCatsApplicationTests.java
+++ src/test/java/com/duoer/campus/CampusCatsApplicationTests.java
@@ -1,13 +1,25 @@
 package com.duoer.campus;
 
+import com.duoer.campus.dao.CatMapper;
+import com.duoer.campus.entity.Cat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class CampusCatsApplicationTests {
 
+    @Autowired
+    private CatMapper catMapper;
     @Test
     void contextLoads() {
+        Cat c = new Cat();
+        c.setCatId(1);
+        c.setCatName("咪咪");
+        List<Cat> cats = catMapper.randomSelect(c);
+        System.out.println(cats);
     }
 
 }
