9e3d4028f2cd587a0a02407bfffb86c421fcd99c
duoer <1557030651@qq.com>
2023-01-25 17:14:48 +0800

    import jwt

--- src/main/resources/static/js/userMsg.js
+++ src/main/resources/static/js/userMsg.js
@@ -1,6 +1,6 @@
 const myAxios = axios.create(
     {
-        baseURL: 'http://localhost:8080/CampusCats'
+        baseURL: 'http://192.168.0.102:8080/CampusCats'
     }
 );
 async function userMsg(title) {
